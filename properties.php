985e55d5ce9b50b34b5233090fbd801bc3fb6cd5
Yong Mei Yan <yongmeiyan0215@gmail.com>
2026-06-02 06:40:46 +0800

    propertie

--- properties.php
+++ properties.php
@@ -784,18 +784,31 @@
       </div>
     </div>
 
+    <!-- Continue for the bottom section -->
+    
+
     <div class="site-footer">
-      <div class="container">
+    <div class="container">
         <div class="row">
           <div class="col-lg-4">
             <div class="widget">
               <h3>Contact</h3>
-              <address>43 Raymouth Rd. Baltemoer, London 3910</address>
+              <address>Ayer Keroh, Melaka</address>
               <ul class="list-unstyled links">
-                <li><a href="tel://11234567890">+1(123)-456-7890</a></li>
-                <li><a href="tel://11234567890">+1(123)-456-7890</a></li>
                 <li>
-                  <a href="mailto:info@mydomain.com">info@mydomain.com</a>
+                  <span class="icon-phone"></span>
+                  <a></a>
+                  <a href="tel://125845236">+60-125845236</a>
+                </li>
+                <li>
+                  <span class="icon-phone"></span>
+                  <a></a>
+                  <a href="tel://1155485623">+60-1155485623</a>
+                </li>
+                <li>
+                  <span class="icon-envelope"></span>
+                  <a></a>
+                  <a href="mailto:adminl@gmail.com">admin@gmail.com</a>
                 </li>
               </ul>
             </div>
@@ -806,20 +819,11 @@
             <div class="widget">
               <h3>Sources</h3>
               <ul class="list-unstyled float-start links">
-                <li><a href="#">About us</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Vision</a></li>
-                <li><a href="#">Mission</a></li>
-                <li><a href="#">Terms</a></li>
-                <li><a href="#">Privacy</a></li>
-              </ul>
-              <ul class="list-unstyled float-start links">
-                <li><a href="#">Partners</a></li>
-                <li><a href="#">Business</a></li>
-                <li><a href="#">Careers</a></li>
-                <li><a href="#">Blog</a></li>
-                <li><a href="#">FAQ</a></li>
-                <li><a href="#">Creative</a></li>
+                <li><a href="about.html">About us</a></li>
+                <li><a href="services.html">Services</a></li>
+                <li><a href="terms.html">Terms</a></li>
+                <li><a href="privacy.html">Privacy</a></li>
+                <li><a href="faq.html">FAQ</a></li>
               </ul>
             </div>
             <!-- /.widget -->
@@ -829,29 +833,20 @@
             <div class="widget">
               <h3>Links</h3>
               <ul class="list-unstyled links">
-                <li><a href="#">Our Vision</a></li>
-                <li><a href="#">About us</a></li>
-                <li><a href="#">Contact us</a></li>
               </ul>
 
               <ul class="list-unstyled social">
                 <li>
-                  <a href="#"><span class="icon-instagram"></span></a>
+                  <a><span class="icon-instagram"></span></a>
                 </li>
                 <li>
-                  <a href="#"><span class="icon-twitter"></span></a>
+                  <a><span class="icon-twitter"></span></a>
                 </li>
                 <li>
-                  <a href="#"><span class="icon-facebook"></span></a>
+                  <a><span class="icon-facebook"></span></a>
                 </li>
                 <li>
-                  <a href="#"><span class="icon-linkedin"></span></a>
-                </li>
-                <li>
-                  <a href="#"><span class="icon-pinterest"></span></a>
-                </li>
-                <li>
-                  <a href="#"><span class="icon-dribbble"></span></a>
+                  <a><span class="icon-linkedin"></span></a>
                 </li>
               </ul>
             </div>
@@ -880,8 +875,10 @@
               <!-- License information: https://untree.co/license/ -->
             </p>
             <div>
-              Distributed by
-              <a href="https://themewagon.com/" target="_blank">themewagon</a>
+             TSE6223 Software Engineering Fundamentals - Group Project
+            </div>
+            <div>
+              Property Rental Management System
             </div>
           </div>
         </div>
